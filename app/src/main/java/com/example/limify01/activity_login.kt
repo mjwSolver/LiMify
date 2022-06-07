4326a378747251a4785601700d43aab73cecc25b
mjwSolver <93109371+mjwSolver@users.noreply.github.com>
2022-06-07 15:48:32 +0700

    setting themes
    declaring colors, getting rid of actionbar and starting login

---
+++ app/src/main/java/com/example/limify01/activity_login.kt
@@ -0,0 +1,15 @@
+package com.example.limify01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_login : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        setContentView(R.layout.activity_login)
+    }
+}
